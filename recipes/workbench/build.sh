d03c6acdd9343db0dbd2cbc43324a759e2991035
Dhananjhay <dhananjhay03@gmail.com>
2025-05-12 20:37:21 -0400

    contingent cmake based on os

--- recipes/workbench/build.sh
+++ recipes/workbench/build.sh
@@ -7,14 +7,23 @@ if [[ "$target_platform" == linux-* ]]; then
     export CXX_FLAGS="$CXX_FLAGS -lGL -lGLU"
 fi
 
-cmake $CMAKE_ARGS -GNinja \
-    -DCMAKE_BUILD_TYPE:STRING=Release \
-    -DCMAKE_INSTALL_PREFIX:STRING=$PREFIX \
-    -DCMAKE_CXX_FLAGS="$CXX_FLAGS" \
-    ../src
+# Base CMake invocation
+cmake_args=(
+  $CMAKE_ARGS
+  -GNinja
+  -DCMAKE_BUILD_TYPE:STRING=Release
+  -DCMAKE_INSTALL_PREFIX:STRING=$PREFIX
+  -DCMAKE_CXX_FLAGS="$CXX_FLAGS"
+)
+
+# Only on macOS do we force policy ≥3.5
+if [[ "$target_platform" == osx-64 ]]; then
+  cmake_args+=( -DCMAKE_POLICY_VERSION:STRING=3.5 )
+fi
+
+# Now run CMake with our assembled args
+cmake "${cmake_args[@]}" ../src
 
 cmake --build .
-
 ctest --extra-verbose --output-on-failure .
-
 cmake --install .
