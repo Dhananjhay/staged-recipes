84b55550f3738dee2d186bb8afa862ba9c03b474
Dhananjhay <dhananjhay03@gmail.com>
2025-05-15 11:09:41 -0400

    try ninja for build

--- recipes/workbench/build.sh
+++ recipes/workbench/build.sh
@@ -7,22 +7,12 @@ if [[ "$target_platform" == linux-* ]]; then
     export CXX_FLAGS="$CXX_FLAGS -lGL -lGLU"
 fi
 
-# Base CMake invocation
-cmake_args=(
-  $CMAKE_ARGS
-  -GNinja
-  -DCMAKE_BUILD_TYPE:STRING=Release
-  -DCMAKE_INSTALL_PREFIX:STRING=$PREFIX
-  -DCMAKE_CXX_FLAGS="$CXX_FLAGS"
-)
+cmake $CMAKE_ARGS -GNinja \
+    -DCMAKE_BUILD_TYPE:STRING=Release \
+    -DCMAKE_INSTALL_PREFIX:STRING=$PREFIX \
+    -DCMAKE_CXX_FLAGS="$CXX_FLAGS" \
+    ../src
 
-# Only on macOS do we force policy ≥3.5
-if [[ "$target_platform" == osx-64 ]]; then
-  cmake_args+=( -DCMAKE_POLICY_VERSION:STRING=3.5 )
-fi
 
-# Now run CMake with our assembled args
-cmake "${cmake_args[@]}" ../src
-
-cmake --build .
-cmake --install .
+ninja
+ninja install
